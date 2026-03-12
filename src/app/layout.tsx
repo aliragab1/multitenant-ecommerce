8f19e0c972fe9c9a68fa593e5e6feb4c91f0faad
aliragab1 <aliragab121@gmail.com>
2026-03-12 05:00:41 +0200

    02: customization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist_Mono, Inter } from 'next/font/google';
+import { DM_Sans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const dmSans = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -21,15 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn(
-        'antialiased',
-        geistMono.variable,
-        'font-sans',
-        inter.variable,
-      )}
-    >
+    <html lang="en" className={cn('antialiased', dmSans.className)}>
       <body>{children}</body>
     </html>
   );
