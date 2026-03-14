8f75d75fc4c19f31c6195493105031378c1802c2
aliragab1 <aliragab121@gmail.com>
2026-03-14 03:57:37 +0200

    04: payload integration

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
+
+const Home = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({ collection: 'categories' });
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+export default Home;
